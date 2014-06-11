9754cb03fdf0db0713bca635b80e5c9bbbe56eef
Aarthika Arunthavanathan <aarthika.nathan.11@cse.mrt.ac.lk>
2014-06-11 11:59:33 +0530

    My second commit

--- Thrift_Store_Program/src/Item.java
+++ Thrift_Store_Program/src/Item.java
@@ -1,14 +1,47 @@
-
 public class Item {
 
-	String itemName; //maximum 15 characters
-	String itemID;
-	double itemPrice; //non negative value
-	int count; //number of items currently in the stock
+	String itemName; //maximum 15 characters 
+	String itemID; 
+	double itemPrice; //non negative value 
+	int count; //number of items currently in the stock 
 	
-	/*Aarthika ^ Shobi*/
-	Item(){
-		count=0;
+	public Item(){ 
+		count=0; 
+		} 
+	
+	public boolean setItemPrice(double price){
+		if(price>=0){
+			itemPrice=price; 
+			return true; 
+		}
+		else 
+			return false; 
+		} 
+	
+	public void setItemName(String itemName){
+		if(itemName.length()==15){
+			this.itemName=itemName; 
+		} 
+		else{ 
+			System.out.println("Invalid item name. Give atmost 15 characters"); 
+		} 
+	} 
+	
+	public boolean setItemID(String itemID){
+		boolean status=validateItemId(itemID);
+		if(status){
+			this.itemID=itemID;
+			return true;
+		} 
+		else 
+			return false;
+		}
+	
 	}
 	
+	private boolean validateItemId(String itemID2) { 
+		// TODO Auto-generated method stubs return false; 
+	} 
+
+}
 }
